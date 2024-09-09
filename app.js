b7c2b26dca13b2fcd7ce176eab642f321716f139
Jason <Kingdomseed@gmail.com>
2024-09-09 12:26:07 +0200

    Updates to JS and a contact button... not sure I want it. And it's not working.

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 window.addEventListener('scroll', () => {
   document.querySelector('.navbar').classList.toggle('nav-scrolled', window.scrollY > 0);
 });
+
+// CONTACT BUTTONS ANIMATION
+const textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach((textButton) => { 
+  let text = textButton.querySelector('p');
+
+  text.innerHTML = text.innerHTML.split('').map((char, index) => '<span style="transform: rotate(${index * 12}deg)">${char}</span>').join('')
+})
